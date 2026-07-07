1909837f106d06726f0899eb0640343fd07d4d2a
Prastav Shrestha <prastavshrestha6@gmail.com>
2026-07-07 14:59:43 +0545

    Generate HTML in cart items

--- Ecommerce-Project/src/pages/HomePage.jsx
+++ Ecommerce-Project/src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import { Header } from '../components/Header.jsx'
+import { FormatMoney } from '../utils/Money.js'
 import './HomePage.css'
 
 export function HomePage({ cart }) {
@@ -46,7 +47,7 @@ export function HomePage({ cart }) {
                                     </div>
 
                                     <div className="product-price">
-                                        ${(product.priceCents / 100).toFixed(2)}
+                                        {FormatMoney(product.priceCents)}
                                     </div>
 
                                     <div className="product-quantity-container">
