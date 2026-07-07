3e2b616fa54b422933fca0718e79b6c215799525
Prastav Shrestha <prastavshrestha6@gmail.com>
2026-07-07 14:29:46 +0545

    first commit

---
+++ Ecommerce-Project/src/App.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { HomePage } from './pages/HomePage'
+import { CheckoutPage } from './pages/CheckoutPage'
+import { Routes, Route } from 'react-router';
+import { OrdersPage } from './pages/OrdersPage';
+import { TrackingPage } from './pages/TrackingPage'
+import './App.css'
+
+function App() {
+
+  const [cart, setCart] = useState([])
+
+  useEffect(() => {
+    axios.get('/api/cart-items')
+      .then((response) => {
+        setCart(response.data)
+      })
+  });
+
+  return (
+    <Routes>
+      <Route index element={<HomePage cart={cart} />} />
+      <Route path="checkout" element={<CheckoutPage cart={cart} />} />
+      <Route path="orders" element={<OrdersPage cart={ cart} />} />
+      <Route path="tracking" element={<TrackingPage cart={cart} />} />
+    </Routes>
+  );
+}
+
+export default App
